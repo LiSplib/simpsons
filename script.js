58d27b37b5f2cabc14b29b892d91d6b450ade037
LiSplib <jcbarret@outlook.fr>
2019-10-11 10:46:16 +0200

    update js

---
+++ script.js
@@ -0,0 +1,186 @@
+var data = [
+    {
+        "album_name": "portrait",
+        "description": "...",
+        "photos": 
+                [
+                    {
+                        "large": "bart.jpg",
+                        "description": "Bartholomet Jo-Jo Simpson dit Bart, le fils terrible, frère de Lisa et Maggie. Il a 10 ans. Il est considéré comme le cancre et le délinquant de la famille. Porte la rumeur d'être aussi intelligent que son père alors qu'il a juste un problème de concentration. Pour preuve il a réussi à mettre plus de dix fois Tahiti Bob en prison. Pourtant, il a un grand cœur. Il est l'ami des minorités. Son pire cauchemar : Tahiti (Sideshow) Bob qu'il a fait emprisonner après un vol au Kwik-E-Mart, mais celui-ci aura l'occasion de lui sauver la vie par la suite. Dans le futur, Bart est guitariste dans un groupe de rock avec Ralph qui ne marche pas, il est divorcé d'avec la mère de ses deux fils et vit encore dans l'école primaire de Springfield.",
+                        "title": "Bart Simpson"
+                    },
+                    {
+                        "large": "homer.jpg",
+                        "description": "Homer J. Simpson dit Homer Simpson, le père, gros, cupide et gourmand. Son intelligence est sous-estimée à cause d'un crayon dans son cerveau. Il adore les donuts et les côtes de porc grillées, le tout arrosé de bière Duff. Il est le chef de sécurité du secteur 7G à la centrale nucléaire de Springfield.", 
+                        "title": "Homer J. Simpson"
+                    },
+                    {
+                        "large": "marge.jpg",
+                        "description": "Marjorie Bouvier dite Marge Simpson, la mère, choucroute bleue sur la tête, être sensible et sensé, c'est elle qui régit toute cette belle petite famille. Élément modéré de la famille, il lui arrive de temps à autre de déraper. Elle a une âme d'artiste.",
+                        "title": "Marge Simpson"
+                    },
+                    {
+                        "large": "lisa.jpg",
+                        "description": "Elisabeth Marie Simpson dite Lisa Simpson, sœur de Bart et Maggie, le génie de la famille. Âgée de seulement 8 ans, très en avance, c'est l'être le plus sensé, le plus adulte de la famille. Elle joue du saxophone. Elle est jalouse de Maggie sûrement plus intelligente qu'elle et c'est grâce à ses bons résultats que l'école primaire de Springfield n'est pas encore fermée. Dans le futur elle est présidente des États-Unis, mariée à Milouse et mère d'une adolescente Zia Simpson.",
+                        "title": "Lisa Simpson"
+                    },
+                    {
+                        "large": "maggi.jpg",
+                        "description": "Margaret Simpson dite Maggie Simpson, la petite dernière, sœur de Bart et Lisa, qui a déjà 2 ans. Elle passe son temps à sucer sa tétine. Elle parlera plusieurs fois dans divers épisodes. Elle aussi est très intelligente malgré son jeune âge, elle a entre autres écrit son nom avec son ardoise magique et E=mc2 avec des cubes, elle sait aussi tirer avec une arme. Dans le futur, Maggie est une chanteuse international mais étant enceinte elle ne parle pas, et elle est aussi mère d'une petite fille Maggie Jr.",
+                        "title": "Maggie Simpson"
+                    },
+                    {
+                        "large": "abe.png",
+                        "description": "Abraham « Abe » Simpson : 85 ans, le père d’Homer, râleur et sénile, vit au « Retirement Castel » (Château de la Retraite), où il a eu une tendre liaison avec une des pensionnaires, Béa, qui malheureusement meurt trop tôt. Il a combattu pour les forces armées américaines au cours de la Seconde Guerre mondiale, d'abord sur le front du Pacifique où il était responsable de la surveillance aérienne à Pearl Harbor avant de devenir aviateur avec pour mitrailleur M. Burns, puis sur le front européen (il aura d'ailleurs une relation sexuelle avec une jeune anglaise la veille de son départ pour le débarquement de Normandie. Leur fille Abbie, issue de cette union, ressemble en tous points à Homer). Il lui arrive également très souvent de s'endormir subitement, même en plein milieu d'un discours. Il a vendu sa maison pour financer l'achat de la maison d'Homer en échange de son hospitalité, mais ce-dernier l'a trahi en l'envoyant dans une maison de retraite.",
+                        "title": "Abraham « Abe » Simpson"
+                    }
+                ]
+            },
+
+            {
+                "album_name": "famille",
+                "description": "...",
+                "photos":
+                [
+                    {
+                        "large": "family.jpg",
+                        "description": "La famille Simpson",
+                        "title": "La famille Simpson"
+                    },
+                    {
+                        "large": "famille.jpg",
+                        "description": "La famille Simpson",
+                        "title": "La famille Simpson"
+                    },
+                    {
+                        "large": "famille2.jpg",
+                        "description": "La famille Simpson",
+                        "title": "La famille Simpson"
+                    }
+                ]
+            },
+
+            {
+                "album_name": "amis",
+                "description": "...",
+                "photos":
+                [
+                    {
+                        "large": "chef wiggum.jpg",
+                        "description": "Clancy, le chef de police incompétent de Springfield ; Il ressemble beaucoup à Homer dans son comportement.",
+                        "title": "Chef Wiggum"
+                    },
+                    {
+                        "large": "duffman.jpg",
+                        "description": "Barry « Duff Man » Duffman, personnage portant en permanence un costume bleu turquoise et rouge, avec une cape rouge, digne d'un super-héros avec une ceinture de canettes de bière de la marque, plus une casquette et des lunettes de soleil sombres. Il est l'incarnation et le principal promoteur de la bière Duff (bien qu'il existe en réalité trois Duffman).",
+                        "title": "Duffman"
+                    },
+                    {
+                        "large": "milous.jpg",
+                        "description": "Milhouse Mussolini Van Houten, fils de la famille, meilleur ami de Bart. Il est amoureux de Lisa et se marie avec elle dans l'épisode Le Futur passé.",
+                        "title": "Milhouse"
+                    },
+                    {
+                        "large": "moe.jpg",
+                        "description": "Moe Szyslak, propriétaire de la taverne, laid et mal-aimé, il veut souvent se suicider : il s'est déjà fait refaire le visage dans un épisode, mais a retrouvé son ancien visage après un accident sur un tournage puis il voulait sauter d'un pont et, par chance, il a rattrapé Maggie Simpson quand Homer a presque foncé dans une voiture (Saison 14 - Épisode 17 : Moe, le baby-sitter). Il est d'origine néerlandaise ou italienne lors d'une apparition de lui étant enfant et parlant avec un accent italien. Il a été un élève brillant à l'université pour barmans de « C'est ma tournée ! » dans Les Maux de Moe (Saison 13 - Épisode 3).",
+                        "title": "Moe Szyslak"
+                    },
+                    {
+                        "large": "ned.png",
+                        "description": "Nedward Ned Flanders, le père, bien intentionné, généreux, fervent chrétien traditionaliste, propriétaire de la boutique Le palais du gaucher (Le Gaucherium au Québec), puis professeur à l'Ecole Elémentaire de Springfield, il est âgé de 60 ans. Bien qu'il ait une apparence chétive, il est en réalité très musclé.",
+                        "title": "La ferme Flanders"
+                    },
+                    {
+                        "large": "apu.jpg",
+                        "description": "Apu, propriétaire et employé à temps plein du Kwik-E-Mart (Mini-marché) de Springfield.",
+                        "title": "Apu Nahasapeemapetilon"
+                    },
+                    {
+                        "large": "barney.jpg",
+                        "description": "Barney Gumble, ancien traqueur de boissons, meilleur ami d'Homer, surdoué jusqu'à la veille de son examen où Homer lui fait découvrir une Bière Duff et devient alors alcoolique et rate sa vie.",
+                        "title": "Barney Gumble"
+                    },
+                    {
+                        "large": "carl.jpg",
+                        "description": "Carl Carlson (autre ami d'Homer de la centrale) : il apparaît presque toujours avec Lenny. Carl n'est pas seulement le collègue d'Homer mais aussi un de ses meilleurs amis depuis l'enfance. Il est noir et beau, bouddhiste, possède une maîtrise en physique nucléaire, adore le bowling et boire un coup chez Moe. Dans l'épisode La Guerre pour les étoiles, il évoque son enfance en Islande. Ce sont pratiquement les seuls détails de sa vie connus. Il a affirmé par le passé qu'il a une famille et qu'il est le supérieur d'Homer, mais il garde la plupart du temps ces choses pour lui, à cause de sa langue éternellement caustique.",
+                        "title": "Carl Carlson"
+                    },
+                    {
+                        "large": "lenny.jpg",
+                        "description": "Lenny Leonard (ami d'Homer de la centrale) : apparaît presque toujours avec Carl. Très populaire auprès de ses proches, chanceux et généreux, Marge, Bart et Lisa l'aiment particulièrement. C'est un fan de bowling et on le retrouve souvent dans le bar de Moe. On découvre dans un épisode de la saison 13 qu'il est bouddhiste, tout comme Carl.",
+                        "title": "Lenny Leonard"
+                    }
+                ]
+
+            },
+            {
+                "album_name": "vilains",
+                "description": "...",
+                "photos":
+                [
+                    {
+                        "large": "burns.png",
+                        "description": "Charles Montgomery Burns : propriétaire, milliardaire machiavélique et assoiffé d'argent.",
+                        "title": "M. Burns"
+                    },
+                    {
+                        "large": "gros_tony.jpg",
+                        "description": "Gros Tony : chef de la mafia de Springfield et souvent accompagné de ses hommes de main. La mafia fait de récurrentes mais rapides apparitions au cours des épisodes, dans lesquelles on la voit cacher des cadavres sans plus d'information. Malgré cela et malgré quelques plus « gros coups » dans d'autres épisodes, Gros Tony et sa bande ne sont que très rarement inquiétés par les policiers, même quand ils « taquinent » ces derniers à l'arme à feu. Gros Tony a déjà aidé Bart, Marge et Homer mais, à chaque fois, son instinct criminel reprend le dessus et cela se termine mal. Il a un fils du nom de Michael et sa chanson préférée est Radio Love. Il meurt dans la saison 22 épisode Gym Tony et est remplacé par son cousin Gym Tony qui deviendra finalement Gros Tony.",
+                        "title": "Gros Tony"
+                    }
+                ]
+            }    
+];
+
+        
+var $thumbnailList = document.getElementById("thumbnail-list");
+
+function onButtonClick(){
+    var $myButton = event.target;
+    var albumName = $myButton.getAttribute("data-album-id");
+
+    var album = findAlbum(albumName);
+    if(album != null){
+        updateSelectedAlbum(album);
+    }
+}
+
+function findAlbum(albumNameToFind){
+    for(var i = 0; i < data.length; i++){
+        var possibleAlbum = data[i];
+        if(possibleAlbum.album_name == albumNameToFind){
+            return data[i];
+        }
+    }
+
+    return null;
+}
+
+function updateSelectedAlbum(newAlbum){
+    $thumbnailList.innerHTML = newAlbum.photos.map(photo => {
+        const href = `./img/${newAlbum.album_name}/${photo.large}`;
+        return `
+        <a href="${href}">
+            <img src="${href}" title="${photo.title}"/>
+        </a>
+        `;
+    }).join("");
+}
+
+function displayPics(){
+    var photos = document.getElementById('thumbnail-list');
+    var liens = photos.getElementsByTagName('a');
+    var main_picture = document.getElementById('main-picture');
+
+    var main_title = document.getElementById('photo').getElementsByTagName('h2')[0];
+    for(var i = 0; i < liens.length; ++i){
+        liens[i].onclick = function(){
+            main_picture.src = this.href;
+            main_picture.alt = this.title;
+            main_title.firstChild.nodeValue = this.title;
+            return false;
+        };
+    }
+}
+
+window.onload = displayPics;
